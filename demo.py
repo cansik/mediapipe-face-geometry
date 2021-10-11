d0d9258b43a9863325dbb438f910dcb6fb6ea237
Florian Bruggisser <florian@bildspur.ch>
2021-10-11 10:33:55 +0200

    implemented basic line display

--- demo.py
+++ demo.py
@@ -1,4 +1,6 @@
 import cv2
+import numpy as np
+
 import mediapipe as mp
 from mpx.face_landmark_with_geometry import FaceLandmarkFrontWithGeometry
 
@@ -16,6 +18,7 @@ while cap.isOpened():
     if not success:
         print("Ignoring empty camera frame.")
         # If loading a video, use 'break' instead of 'continue'.
+        cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
         continue
 
     # Flip the image horizontally for a later selfie-view display, and convert
@@ -39,10 +42,24 @@ while cap.isOpened():
                 connection_drawing_spec=drawing_spec)
 
     if results.multi_face_geometry:
-        for geometry in results.multi_face_geometry:
-            print(geometry.pose_transform_matrix)
+        for i, geometry in enumerate(results.multi_face_geometry):
+            raw_mat = geometry.pose_transform_matrix
+            pose_mat = np.asarray(raw_mat.packed_data).reshape(raw_mat.cols, raw_mat.rows)
+            h, w = image.shape[:2]
+
+            p = np.asarray([0, 0, 0.2])
+            pp = p.dot(pose_mat[:3, :3])
+
+            center = results.multi_face_landmarks[i].landmark[1]
+
+            x = (pp[0] + center.x) * w
+            y = (-pp[1] + center.y) * h
+
+            cv2.line(image, (round(center.x * w), round(center.y * h)),
+                     (round(x), round(y)),
+                     color=(255, 0, 255), thickness=2)
 
     cv2.imshow('MediaPipe FaceMesh', image)
-    if cv2.waitKey(5) & 0xFF == 27:
+    if cv2.waitKey(20) & 0xFF == 27:
         break
 cap.release()
